3cce1f60f132c72a4a7834dcbd38e442a020f04f
Julieta <julieta.digifico@gmail.com>
2024-02-12 06:05:42 -0300

    3bis ejercicio if

---
+++ app.py
@@ -0,0 +1,47 @@
+import tkinter
+from tkinter.messagebox import showinfo as alert
+from tkinter.messagebox import askyesno as question
+from tkinter.simpledialog import askstring as prompt
+import customtkinter
+
+'''
+nombre: Julieta
+apellido: Digifico
+---
+Ejercicio: if_03bis
+---
+Enunciado:
+A partir del ingreso de la altura de un basquetbolista determinar si es pivot o no. Para serlo el mismo deberá medir mas de 1.80 metros
+'''
+
+
+class App(customtkinter.CTk):
+
+    def __init__(self):
+        super().__init__()
+
+        self.title("UTN FRA")
+
+        self.label1 = customtkinter.CTkLabel(master=self, text="Altura")
+        self.label1.grid(row=0, column=0, padx=20, pady=10)
+
+        self.txt_altura = customtkinter.CTkEntry(master=self)
+        self.txt_altura.grid(row=0, column=1)
+
+        self.btn_mostrar = customtkinter.CTkButton(
+            master=self, text="Mostrar", command=self.btn_mostrar_on_click)
+        self.btn_mostrar.grid(row=2, pady=20, columnspan=2, sticky="nsew")
+
+    def btn_mostrar_on_click(self):
+        altura_txt = self.txt_altura.get()
+        altura = float(altura_txt)
+        if altura > 1.80:
+            alert ("Posición", "El jugador es pivot")
+        else:
+            alert ("Posición", "El jugador no es pivot")
+
+
+if __name__ == "__main__":
+    app = App()
+    app.geometry("300x300")
+    app.mainloop()
